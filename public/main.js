5d9ab8b64356e661dc3b4f3b3895935798c2f922
Young <wyoung@proforma.com>
2018-04-02 21:06:11 -0400

    Jeopardy

--- public/main.js
+++ public/main.js
@@ -1,5 +1,59 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+var BASE_URL = "http://jservice.io";
 
-document.addEventListener('DOMContentLoaded', main)
+var categoriesUrl = "/api/categories?count=5";
+
+var categoryUrl = "/api/category?id={0}";
+
+angular
+  .module("mainApp", [])
+  .controller("mainController", ['$scope', '$http', ($scope, $http) => {
+
+    $scope.clueSelected = false;
+    $scope.currentScore = 0;
+    $scope.cancel = () => {
+      $scope.clueSelected = false;
+    };
+    $scope.update = (answer) => {
+      console.log(answer);
+      if (answer == $scope.selectedClue.answer) {
+        console.log("CORRECT");
+        $scope.currentScore += $scope.selectedClue.value;
+        $scope.clueSelected = false;
+      } else {
+        $scope.currentScore -= $scope.selectedClue.value;
+        $scope.clueSelected = false;
+      }
+    };
+
+    $http({
+      method: "GET",
+      url: BASE_URL + categoriesUrl
+    }).then(response => {
+
+      $scope.categories = response.data;
+
+      $scope.categories.forEach(category => {
+
+        $http({
+          method: "GET",
+          url: BASE_URL + categoryUrl.replace("{0}", category.id)
+        }).then(response => {
+
+          category.clues = response.data.clues;
+        });
+
+      });
+    });
+
+    $scope.selectClue = (clue) => {
+
+      $scope.selectedClue = clue;
+      $scope.clueSelected = true;
+    };
+
+    $scope.hideClue = () => {
+
+      $scope.clueSelected = false;
+    };
+
+  }]);
